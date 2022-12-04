a793c54268b105756d25acdaa2da4d62c6c2ae32
FarazBaghernezhad <fbaghernezhad11788@ada.edu.az>
2022-12-04 20:15:43 +0400

    classes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let weather = {
     "apiKey": "4ee540491ce6d9ece7b364f735f8d0fc",
     fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/geo/1.0/direct?q="
+            "https://api.openweathermap.org/data/2.5/weather?q="
              + city
              + "&appid="
              + this.apiKey
@@ -12,6 +12,11 @@ let weather = {
         .then((data) => console.log(data));
     },
     displayWeather: function(data) {
+        const { name } = data;
+        const {icon, description } = data.weather;
+        const {temp, humidity, pressure, temp_min, temp_max, feels_like} = data.main;
+        const {speed} = data.wind;
+        
 
     }
 
